ad6c8e84646716f34e533eef50ffb9757197405a
sushant shinde <shindesushant9979@hotmail.com>
2021-01-08 15:42:32 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='OrderBook',
+    version='0.1.2',
+    author='Michael Nguyen',
+    author_email='dyn4mik3@gmail.com',
+    packages=['orderbook'],
+    url='http://pypi.python.org/pypi/OrderBook/',
+    license='LICENSE.txt',
+    description='Limit Order Book Matching Engine',
+    install_requires=[
+        'sortedcontainers >= 2'
+        ],
+    scripts=['bin/algosim.py']
+    )
